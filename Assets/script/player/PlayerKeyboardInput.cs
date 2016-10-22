873919187d28f65d74f00a876f5681134904c8ea
vanomart <martinvano@martinvano.sk>
2016-10-23 00:17:28 +0200

    Player script minor refactoring

--- Assets/script/player/PlayerKeyboardInput.cs
+++ Assets/script/player/PlayerKeyboardInput.cs
@@ -1,28 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(CharacterController))]
-public class Player: MonoBehaviour {
+public class PlayerKeyboardInput : MonoBehaviour {
 
 	public float runSpeed = 3.0f;
-
-	public enum RotationAxes {
-		MouseXAndY = 0,
-		MouseX = 1,
-		MouseY = 2
-	}
-
-	public RotationAxes axes = RotationAxes.MouseXAndY;
-	public float sensitivityX = 15F;
-	public float sensitivityY = 15F;
-
-	public float minimumX = -360F;
-	public float maximumX = 360F;
-
-	public float minimumY = -60F;
-	public float maximumY = 60F;
-
-	float rotationY = 0F;
 
 	void Update() {
 		// part 1 - move character
