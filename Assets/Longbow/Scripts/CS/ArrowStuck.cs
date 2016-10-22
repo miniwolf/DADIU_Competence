c3f813bd2ed0dba69191b8499d5f726613265204
vanomart <martinvano@martinvano.sk>
2016-10-23 00:13:50 +0200

    Working bow and arrow

---
+++ Assets/Longbow/Scripts/CS/ArrowStuck.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowStuck : MonoBehaviour {
+
+	RaycastHit hit;
+	public AudioClip hitSound; //sound to play when arrow hits a surface
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Only check for obstacles to stick to if the arrow is moving
+		if(GetComponent<Rigidbody>().velocity.magnitude > 0.5) {
+			CheckForObstacles();
+		}
+		else {
+			enabled = false;
+		}
+	}
+
+	void CheckForObstacles() {
+		//the length of the raycast needs to be greater the faster the arrow is traveling, in order for it to register
+		float myVelocity = GetComponent<Rigidbody>().velocity.magnitude;
+		//get length of raycast based on velocity of arrow | based on this, arrows with too little of a velocity will not stick
+		float raycastLength = myVelocity * 0.03f;
+		
+		if(Physics.Raycast(transform.position, transform.forward, out hit, raycastLength)) {
+			GetComponent<AudioSource>().Stop();
+			GetComponent<AudioSource>().clip = hitSound;
+			GetComponent<AudioSource>().Play();
+			
+			//since we need to disable the boxCollider and freeze the rigidbody for the arrow to stick,
+			//we must add our own force to any moveable objects (objects with rigidbodies) that the arrow hits
+			if(hit.transform.GetComponent<Rigidbody>()) {
+				//add force to object hit
+				hit.transform.GetComponent<Rigidbody>().AddForce(transform.forward * myVelocity * 10);
+			}
+			
+			//disable arrow's collider | if you leave the arrow's collider on, while inside another object, it may have strange effects
+			GetComponent<BoxCollider>().enabled = false;
+
+			//if you require the boxCollider to remain active, say for picking up the arrow, you can instead set the collider to trigger:
+			//GetComponent(BoxCollider).isTrigger = true;
+			
+			//freeze rigidbody
+			GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+			//position arrow
+			transform.position = hit.point;
+			//parent arrow to object | if the object the arrow hits moves, this will make the arrow move with it
+			transform.parent = hit.transform;
+			
+			//once the arrow is stuck, disable this script
+			enabled = false;
+		}
+		else {
+			//make arrows top-heavy | on arrows with little velocity (when the bow is not drawn back very far) the arrows will rotated toward the ground
+			Quaternion newRot = transform.rotation;
+			newRot.SetLookRotation(GetComponent<Rigidbody>().velocity);
+			transform.rotation = newRot;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if(GetComponent<Rigidbody>().velocity.magnitude > 5) {
+			if(collision.transform.tag != "Player") {
+				GetComponent<BoxCollider>().enabled = false;
+
+				//freeze rigidbody
+				GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+				//position arrow
+				foreach(ContactPoint contact in collision.contacts) {
+					Vector3 pos = contact.point;
+					transform.position = pos;
+				}
+				//parent arrow to object | if the object the arrow hits moves, this will make the arrow move with it
+				transform.parent = collision.transform;
+				
+				enabled = false;
+			}
+		}
+	}
+}
---
+++ Assets/Longbow/Scripts/CS/LongbowShoot.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LongbowShoot : MonoBehaviour {
+
+	public Transform arrowSpawn; //the arrow's spawn point
+	public Transform projectile; //the arrow to be instantiated
+	public AudioClip drawSound; //sound to be played when drawing the bow (when the string is pulled back)
+	public AudioClip shootSound; //sound to play when arrow is shot
+	int maxPower = 2000; //max force applied to the arrow
+	float power; //how far the arrow will be shoot
+	public float destroyTime = 10; //destroy the instantiated arrow, after this many seconds | if destroyArrows is unchecked, this time will have no effect
+	public bool destroyArrows = false; //destroy arrows shortly after they've been shot, or not
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			//play drawSound
+			GetComponent<AudioSource>().Stop();
+			GetComponent<AudioSource>().clip = drawSound;
+			GetComponent<AudioSource>().volume = 1;
+			GetComponent<AudioSource>().Play();
+			
+			//Play Draw animation On Mouse Down
+			GetComponent<Animation>().Play("Draw");
+			GetComponent<Animation>()["Draw"].speed = 1;
+			GetComponent<Animation>()["Draw"].wrapMode = WrapMode.Once;
+			
+			//Enable arrowSpawn MeshRenderer
+			arrowSpawn.transform.GetComponent<Renderer>().enabled = true;
+
+			//reset power to 0
+			power = 0;
+		}
+		if(Input.GetMouseButton(0)) {
+			//Increase power of shot
+			if(power < maxPower) { //power will not exceed 2000
+				power += maxPower * Time.deltaTime; //add 2000 to power every second
+			}
+		}
+		if(Input.GetMouseButtonUp(0)) {
+			//Calculate Shoot animation Time
+			//This way, if the player lets go of the mouse before the entire Draw animation is complete, 
+			//the Shoot animation isn't played from the begining (where the the string is drawn all the way back)
+			float percent = GetComponent<Animation>()["Draw"].time / GetComponent<Animation>()["Draw"].length;
+			float shootTime = 1 * percent;
+			
+			//play shootSound
+			GetComponent<AudioSource>().Stop();
+			GetComponent<AudioSource>().time = 0.2f;
+			GetComponent<AudioSource>().clip = shootSound;
+			if(percent == 0) {
+				GetComponent<AudioSource>().volume = 1;
+			}
+			else {
+				//volume of sound will be determined by arrow's power
+				GetComponent<AudioSource>().volume = 1 * percent;
+			}
+			GetComponent<AudioSource>().Play();
+			
+			//Play Shoot animation On Mouse Up
+			GetComponent<Animation>().Play("Shoot");
+			GetComponent<Animation>()["Shoot"].speed = 1;
+			GetComponent<Animation>()["Shoot"].time = shootTime;
+			GetComponent<Animation>()["Shoot"].wrapMode = WrapMode.Once;
+			
+			//Disable arrow Spawn MeshRenderer
+			arrowSpawn.transform.GetComponent<Renderer>().enabled = false;
+			
+			//Instantiated projectile (arrow)
+			Transform arrow = Instantiate(projectile, arrowSpawn.transform.position, transform.rotation) as Transform;
+			
+			//Add force to projectile, based off power
+			arrow.transform.GetComponent<Rigidbody>().AddForce(transform.forward * power);
+			
+			if(destroyArrows == true) {
+				//Destroy instantiated arrow, after given time
+				Destroy(arrow.gameObject, destroyTime); //< you can change the amount of time until the arrow is destroyed by chaning destroyTime on the script, in the editor
+			}
+		}
+	}
+}
